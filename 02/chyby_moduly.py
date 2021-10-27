66ab8f5f1c892dd39c0225d064614481b8a2a83b
pcechmanek <patrik.cechmanek@ceosdata.com>
2021-10-27 17:54:16 +0200

    initial commit chyby_moduly

---
+++ 02/chyby_moduly.py
@@ -0,0 +1,39 @@
+while True:
+    num1 = input("Zadej první číslo: ")
+    num2 = input("Zadej druhé číslo: ")
+    try:
+        int_num1 = int(num1)
+        int_num2 = int(num2)
+        break
+    except ValueError:
+        print("Zadávej pouze čísla")
+
+
+oper1 = input("Zadej operátor +, -, * nebo /.")
+operator = ["+", "-", "*", "/"]
+vysledek = ""
+
+
+def deleni(delenec, delitel):
+    try:
+        return delenec / delitel
+    except ZeroDivisionError:
+        print(f'{delitel} nelze dělit.')
+
+
+if oper1 == operator[0]:
+    vysledek = int_num1 + int_num2
+elif oper1 == operator[1]:
+    vysledek = int_num1 - int_num2
+elif oper1 == operator[2]:
+    vysledek = int_num1 * int_num2
+else:
+    if int_num2 == 0:
+        vysledek = "Nulou nelze delit"
+    else:
+        vysledek = deleni(int_num1, int_num2)
+
+print("První číslo: ", int_num1)
+print("Druhé číslo: ", int_num2)
+print("Operace: ", oper1)
+print(int_num1, oper1, int_num2, " = ", vysledek)
