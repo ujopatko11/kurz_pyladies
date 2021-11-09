f67df38093ab0059c5d1942eddf3c15c78a79b55
pcechmanek <patrik.cechmanek@ceosdata.com>
2021-11-09 19:49:25 +0100

    ukol 1, 2

--- 09/list_zvirata.py
+++ 09/list_zvirata.py
@@ -1,5 +1,22 @@
 def vytvor_seznam_zvirat():
     return ['pes', 'kočka', 'králík', 'had']
 zvirata = vytvor_seznam_zvirat()
-zvirata.pop()
-print(zvirata)
+#print(zvirata)
+
+
+def filtruj_kratka_jmena(list, size):
+    result = []
+    for word in list:
+        if len(word) < size:
+            result.append(word)
+    return result
+print(filtruj_kratka_jmena(zvirata, 5))
+
+def filtruj_k(list):
+    result = []
+    for word in list:
+        if word.startswith("k"):
+            result.append(word)
+    return result
+print(filtruj_k(zvirata))
+
