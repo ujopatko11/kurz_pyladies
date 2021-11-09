646e9333d117fbf7bbb8ba6b4b9884df600b8705
pcechmanek <patrik.cechmanek@ceosdata.com>
2021-11-09 20:01:46 +0100

    ukol 3

--- 09/list_zvirata.py
+++ 09/list_zvirata.py
@@ -10,7 +10,7 @@ def filtruj_kratka_jmena(list, size):
         if len(word) < size:
             result.append(word)
     return result
-print(filtruj_kratka_jmena(zvirata, 5))
+#print(filtruj_kratka_jmena(zvirata, 5))
 
 def filtruj_k(list):
     result = []
@@ -18,5 +18,13 @@ def filtruj_k(list):
         if word.startswith("k"):
             result.append(word)
     return result
-print(filtruj_k(zvirata))
+#print(filtruj_k(zvirata))
+
+def obsahuje(list, word):
+    if word in list:
+        return True
+    else:
+        return False
+print(obsahuje(zvirata, "pes"))
+print(obsahuje(zvirata, "nabytek"))
 
