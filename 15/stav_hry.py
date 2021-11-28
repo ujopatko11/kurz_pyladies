433ed6ed2c5ee0ffed4bf11125f8e44744ec7dae
pcechmanek <patrik.cechmanek@ceosdata.com>
2021-11-28 19:44:56 +0100

    update formating json

--- 15/stav_hry.py
+++ 15/stav_hry.py
@@ -6,6 +6,6 @@ data = {
 }
 def zapis_stav(koren, hodnota):
     data[koren].append(hodnota)
-    run = json.dumps(data)
+    run = json.dumps(data, ensure_ascii=False, indent=2)
     with open('stavHry.json', encoding='utf-8', mode='w') as souborJson:
         print(run, file=souborJson)
