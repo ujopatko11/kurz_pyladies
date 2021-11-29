f5867e52c3b6b498ef5639fe4e9f3eb4abb477c8
pcechmanek <patrik.cechmanek@ceosdata.com>
2021-11-29 17:51:16 +0100

    hotfix

--- 15/stav_hry.py
+++ 15/stav_hry.py
@@ -5,21 +5,20 @@ data = {
     'vitez': []
 }
 def zapis_stav(koren, hodnota):
-    data[koren].append(hodnota)
+    data[koren] = hodnota
     run = json.dumps(data, ensure_ascii=False, indent=2)
     with open('stavHry.json', encoding='utf-8', mode='w') as souborJson:
         print(run, file=souborJson)
 
 def zjisti_stav_hry():
-
-    with open('stavHry.json', encoding='utf-8', mode='r') as souborJsonRead:
-        kod = souborJsonRead.read()
-        data = json.loads(kod)
-        if data['vitez'] == []:
-            return data['stav_hry'][-1]
-        else:
-            return "-" * 20
-
+    try:
+        with open('stavHry.json', encoding='utf-8', mode='r') as souborJsonRead:
+            kod = souborJsonRead.read()
+            data = json.loads(kod)
+            if data['vitez'] == []:
+                return data['stav_hry'][-1]
+    except FileNotFoundError:
+        return "-" * 20
 
 
 
